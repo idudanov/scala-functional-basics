b9a1e7e6c1bba1d97e9432d75ee00730e26d7544
Ivan Dudanov <idudanov@l3355.seek.int>
2018-04-12 16:14:57 +1000

    clean up

--- src/main/scala/fpbasics/KleisliTransformer.scala
+++ src/main/scala/fpbasics/KleisliTransformer.scala
@@ -3,6 +3,18 @@ package fpbasics
 import cats.data.Kleisli
 import cats.implicits._
 
+
+/**
+  * Kleisli can be viewed as the monad transformer for functions. Recall that at its essence, Kleisli[F, A, B] is just a
+  * function A => F[B], with niceties to make working with the value we actually care about, the B, easy. Kleisli allows
+  * us to take the effects of functions and have them play nice with the effects of any other F[_].
+  *
+  * In other words lets transform UserA into UserB
+  *
+  * UserA => UserB
+  *
+  */
+
 case class UserA(firstname: String, lastname: String)
 
 case class UserB(givenname: String, surname: Int)
@@ -24,7 +36,16 @@ object KleisliTransformer extends App {
   def readSecond = Kleisli[Result, UserA, Int] {
     userA => Right(userA.lastname.hashCode)
   }
-
+  
+  /**
+    * To repeat:
+    *
+    * Kleisli[F[_], A, B] which is just A => F[B]
+    *
+    *             Monad(F[_]) In(A)  Out(B)
+    *                   |       |      |
+    *                   v       v      v
+    */
   def mainFn: Kleisli[Result, UserA, UserB] = for {
     givenname <- readFirst
     surname   <- readSecond
